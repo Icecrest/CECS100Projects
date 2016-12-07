6ecbb459edd7023d4f6a02bd2ef0c562eee3c5a6
Sean Cuurley <seancurley007@gmail.com>
2016-12-06 23:55:04 -0800

    So Many Updates.  Fix to a lot of things.  changed "Claw" to "Tree" in all instances.  Tree is now finished and operational.

--- Project 1/start/Chapter 13/Lab10_Num3.py
+++ Project 1/start/Chapter 13/Lab10_Num3.py
@@ -1,106 +1,369 @@
 """Cipher Encryptor and Decrypter"""
 
 file = open('Message', 'r+')
-original = file.readlines
-message = file.readlines()
-newmessage = ""
+file.seek(0)
+original = file.readlines().pop(0)
+message = []
+message_text = ""
+is_readable = True
 
 
 # Encrypt
 def encrypt(type):
     if type == "caesar":
         caesar()
-    elif type == "claw":
-        claw()
+    elif type == "tree":
+        tree()
+
 
 # Decrypt
 def decrypt(type):
-    print("unfinished")
+    if type == "caesar":
+        de_caesar()
+    elif type == "tree":
+        de_tree()
+
 
 # Cipher Selection
-def select(choice):
-    run = True
-    while run:
-        selection = input("What's poppin Jimbo?\n- Caesar Shift\n- Claw")
-        if choice == "encrypt":
-            encrypt(selection)
-            run = False
-        if choice == "decrypt":
-            decrypt(selection)
-            run = False
+def select():
+    master = True
+    while master:
+        run = True
+        selection = input("What's poppin Jimbo?\n- Encrypt\n- Decrypt\n- Display\n- Quit Program")
+        if selection == "encrypt":
+            while run:
+                selection = input("What's poppin Jimbo?\n- Caesar\n- Tree\n- Display Text\n- Quit")
+                if selection == "tree" or selection == "caesar":
+                    encrypt(selection)
+                    run = False
+                elif selection == "display":
+                    display()
+                    run = False
+                elif selection == "quit":
+                    return 0
+                else:
+                    print("Invalid input, please try again.\n*\n*")
+        elif selection == "decrypt":
+            while run:
+                selection = input("What's poppin Jimbo?\n- Caesar\n- Tree\n- Display Text\n- Quit")
+                if selection == "tree" or selection == "caesar":
+                    decrypt(selection)
+                    run = False
+                elif selection == "display":
+                    display()
+                    run = False
+                elif selection == "quit":
+                    return 0
+                else:
+                    print("Invalid input, please try again.\n*\n*")
+        elif selection == "display":
+            display()
+        elif selection == "quit":
+            return 0
+        else:
+            print("Invalid input, please try again.\n*\n*")
 
-# Display File Text - Anfernee
+# Display file text
 def display():
-    print("Original Message:",message)
-    if not message == original:
-        print("New Message:",message)
+    if not message_text == "":
+        if is_readable:
+            print(message_text)
+    else:
+        print("There is no text to display yet")
 
-# Enigma Creator -
 
 # Caesar Shift Creator -
 def caesar():
-    print(message)
+    global original
+    global message
+    global message_text
+    global is_readable
+    letter_shift = {}
+    letter_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                  'u', 'v', 'w', 'x', 'y', 'z']
+    placeholder = []
+    place = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+             'u', 'v', 'w', 'x', 'y', 'z']
+    a_equals = input("What letter do you want to shift 'a' to?")
+    for i in range(0, letter_set.index(a_equals)):
+        placeholder.append(letter_set[i])
+        del letter_set[i]
+    letter_set.extend(placeholder)
+    for item in place:
+        letter_shift[item] = letter_set[place.index(item)]
+    print(letter_shift)
+    """for letter in original:
+        if letter == "a":
+            message.append(letter_shift.pop(0))
+        elif letter == "b":
+            message.append(letter_shift.pop(1))
+        elif letter == "c":
+            message.append(letter_shift.pop(2))
+        elif letter == "d":
+            message.append(letter_shift.pop(3))
+        elif letter == "e":
+            message.append(letter_shift.pop(4))
+        elif letter == "f":
+            message.append(letter_shift.pop(5))
+        elif letter == "g":
+            message.append(letter_shift.pop(6))
+        elif letter == "h":
+            message.append(letter_shift.pop(7))
+        elif letter == "i":
+            message.append(letter_shift.pop(8))
+        elif letter == "j":
+            message.append(letter_shift.pop(9))
+        elif letter == "k":
+            message.append(letter_shift.pop(10))
+        elif letter == "l":
+            message.append(letter_shift.pop(11))
+        elif letter == "m":
+            message.append(letter_shift.pop(12))
+        elif letter == "n":
+            message.append(letter_shift.pop(13))
+        elif letter == "o":
+            message.append(letter_shift.pop(14))
+        elif letter == "p":
+            message.append(letter_shift.pop(15))
+        elif letter == "q":
+            message.append(letter_shift.pop(16))
+        elif letter == "r":
+            message.append(letter_shift.pop(17))
+        elif letter == "s":
+            message.append(letter_shift.pop(18))
+        elif letter == "t":
+            message.append(letter_shift.pop(19))
+        elif letter == "u":
+            message.append(letter_shift.pop(20))
+        elif letter == "v":
+            message.append(letter_shift.pop(21))
+        elif letter == "w":
+            message.append(letter_shift.pop(22))
+        elif letter == "x":
+            message.append(letter_shift.pop(23))
+        elif letter == "y":
+            message.append(letter_shift.pop(24))
+        elif letter == "z":
+            message.append(letter_shift.pop(25))
+        elif letter == " ":
+            message.append(" ")
+        """
+    message_text = message_text.join(message)
+    is_readable = True
+    print("*\nCaesar Shift Encrypted\n*")
+
+
+def de_caesar():
+    global message
+    global message_text
+    global is_readable
+    letter_shift = {
+        'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e', 'f': 'f', 'g': 'g', 'h': 'h', 'i': 'i', 'j': 'j',
+        'k': 'k', 'l': 'l', 'm': 'm', 'n': 'n', 'o': 'o', 'p': 'p', 'q': 'q', 'r': 'r', 's': 's', 't': 't',
+        'u': 'u', 'v': 'v', 'w': 'w', 'x': 'x', 'y': 'y', 'z': 'z'}
+    placeholder = []
+    a_equals = input("What letter does 'a' shift to?")
+    for i in range(0, letter_shift.index(a_equals)):
+        placeholder.append(letter_shift.pop(i))
+    for element in placeholder:
+        letter_shift.append(element)
+    for letter in message_text:
+        if letter == "a":
+            message.append(letter_shift.pop(0))
+        elif letter == "b":
+            message.append(letter_shift.pop(1))
+        elif letter == "c":
+            message.append(letter_shift.pop(2))
+        elif letter == "d":
+            message.append(letter_shift.pop(3))
+        elif letter == "e":
+            message.append(letter_shift.pop(4))
+        elif letter == "f":
+            message.append(letter_shift.pop(5))
+        elif letter == "g":
+            message.append(letter_shift.pop(6))
+        elif letter == "h":
+            message.append(letter_shift.pop(7))
+        elif letter == "i":
+            message.append(letter_shift.pop(8))
+        elif letter == "j":
+            message.append(letter_shift.pop(9))
+        elif letter == "k":
+            message.append(letter_shift.pop(10))
+        elif letter == "l":
+            message.append(letter_shift.pop(11))
+        elif letter == "m":
+            message.append(letter_shift.pop(12))
+        elif letter == "n":
+            message.append(letter_shift.pop(13))
+        elif letter == "o":
+            message.append(letter_shift.pop(14))
+        elif letter == "p":
+            message.append(letter_shift.pop(15))
+        elif letter == "q":
+            message.append(letter_shift.pop(16))
+        elif letter == "r":
+            message.append(letter_shift.pop(17))
+        elif letter == "s":
+            message.append(letter_shift.pop(18))
+        elif letter == "t":
+            message.append(letter_shift.pop(19))
+        elif letter == "u":
+            message.append(letter_shift.pop(20))
+        elif letter == "v":
+            message.append(letter_shift.pop(21))
+        elif letter == "w":
+            message.append(letter_shift.pop(22))
+        elif letter == "x":
+            message.append(letter_shift.pop(23))
+        elif letter == "y":
+            message.append(letter_shift.pop(24))
+        elif letter == "z":
+            message.append(letter_shift.pop(25))
+    message_text = message_text.join(message)
+    is_readable = True
+    print("*\nCaesar Shift Decrypted\n*")
+
 
 # Claw Creator - Sean/Anfernee
-def claw():
-    for i in message:
+def tree():
+    global original
+    global message
+    global is_readable
+    characters = []
+    for letter in original.lower():
+        characters.append(letter)
+    for i in characters:
         if i == 'a':
-            message[i] = "|"
+            characters[characters.index(i)] = "|"
         elif i == 'b':
-            message[i] = "|\\"
+            characters[characters.index(i)] = "|\\"
         elif i == 'c':
-            message[i] = "||"
+            characters[characters.index(i)] = "||"
         elif i == 'd':
-            message[i] = "|//"
+            characters[characters.index(i)] = "|/"
         elif i == 'e':
-            message[i] = "\\"
+            characters[characters.index(i)] = "\\"
         elif i == 'f':
-            message[i] = "||\\"
+            characters[characters.index(i)] = "||\\"
         elif i == 'g':
-            message[i] = "|||"
+            characters[characters.index(i)] = "|||"
         elif i == 'h':
-            message[i] = "||/"
+            characters[characters.index(i)] = "||/"
         elif i == 'i':
-            message[i] = "/"
+            characters[characters.index(i)] = "/"
         elif i == 'j':
-            message[i] = "|\\\\"
+            characters[characters.index(i)] = "|\\\\"
         elif i == 'k':
-            message[i] = "|\\|"
+            characters[characters.index(i)] = "|\\|"
         elif i == 'l':
-            message[i] = "|\\/"
+            characters[characters.index(i)] = "|\\/"
         elif i == 'm':
-            message[i] = "|/\\"
+            characters[characters.index(i)] = "|/\\"
         elif i == 'n':
-            message[i] = "|/|"
+            characters[characters.index(i)] = "|/|"
         elif i == 'o':
-            message[i] = "|//"
+            characters[characters.index(i)] = "|//"
         elif i == 'p':
-            message[i] = "\\\\"
+            characters[characters.index(i)] = "\\\\"
         elif i == 'q':
-            message[i] = "\\|"
+            characters[characters.index(i)] = "\\|"
         elif i == 'r':
-            message[i] = "\\/"
+            characters[characters.index(i)] = "\\/"
         elif i == 's':
-            message[i] = "/\\"
+            characters[characters.index(i)] = "/\\"
         elif i == 't':
-            message[i] = "/|"
+            characters[characters.index(i)] = "/|"
         elif i == 'u':
-            message[i] = "//"
+            characters[characters.index(i)] = "//"
         elif i == 'v':
-            message[i] = "||\\\\"
+            characters[characters.index(i)] = "||\\\\"
         elif i == 'w':
-            message[i] = "||//"
+            characters[characters.index(i)] = "||//"
         elif i == 'x':
-            message[i] = "|||\\"
+            characters[characters.index(i)] = "|||\\"
         elif i == 'y':
-            message[i] = "|||/"
+            characters[characters.index(i)] = "|||/"
         elif i == 'z':
-            message[i] = "||||"
+            characters[characters.index(i)] = "||||"
+    message = characters
+    print(message)
+    print("*\nTree Code Encrypted\n*")
+    is_readable = False
+
+
+# Claw Decryptor - Sean
+def de_tree():
+    global message
+    global message_text
+    global is_readable
+    letters = []
+    for element in message:
+        letters.append(element)
+    for i in letters:
+        if i == '|':
+            letters[letters.index(i)] = "a"
+        elif i == '|\\':
+            letters[letters.index(i)] = "b"
+        elif i == '||':
+            letters[letters.index(i)] = "c"
+        elif i == '|/':
+            letters[letters.index(i)] = "d"
+        elif i == '\\':
+            letters[letters.index(i)] = "e"
+        elif i == '||\\':
+            letters[letters.index(i)] = "f"
+        elif i == '|||':
+            letters[letters.index(i)] = "g"
+        elif i == '||/':
+            letters[letters.index(i)] = "h"
+        elif i == '/':
+            letters[letters.index(i)] = "i"
+        elif i == '|\\\\':
+            letters[letters.index(i)] = "j"
+        elif i == '|\\|':
+            letters[letters.index(i)] = "k"
+        elif i == '|\\/':
+            letters[letters.index(i)] = "l"
+        elif i == '|/\\':
+            letters[letters.index(i)] = "m"
+        elif i == '|/|':
+            letters[letters.index(i)] = "n"
+        elif i == '|//':
+            letters[letters.index(i)] = "o"
+        elif i == '\\\\':
+            letters[letters.index(i)] = "p"
+        elif i == '\\|':
+            letters[letters.index(i)] = "q"
+        elif i == '\\/':
+            letters[letters.index(i)] = "r"
+        elif i == '/\\':
+            letters[letters.index(i)] = "s"
+        elif i == '/|':
+            letters[letters.index(i)] = "t"
+        elif i == '//':
+            letters[letters.index(i)] = "u"
+        elif i == '||\\\\':
+            letters[letters.index(i)] = "v"
+        elif i == '||//':
+            letters[letters.index(i)] = "w"
+        elif i == '||//':
+            letters[letters.index(i)] = "x"
+        elif i == '|||/':
+            letters[letters.index(i)] = "y"
+        elif i == '||||':
+            letters[letters.index(i)] = "z"
+    message = letters
+    message_text = message_text.join(message)
+    print("*\nTree Decrypted\n*")
+    is_readable = True
+
 
 def main():
-    display()
-    select("encrypt")
-    display()
+    check = 1
+    while check:
+        check = select()
+    print("\n\n\n\n~~~~~~~~~~~ END OF PROGRAM ~~~~~~~~~~~")
 
 
 main()
