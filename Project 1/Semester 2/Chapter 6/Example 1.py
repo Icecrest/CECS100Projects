5efee7d00e0f3e5b098ddaac53503372fcd6c8dc
Icecrest <seancurley007@gmail.com>
2017-03-13 21:09:43 -0700

    New File

---
+++ Project 1/Semester 2/Chapter 6/Example 1.py
@@ -0,0 +1,28 @@
+"""
+    Create a Text file
+    Populate with numbers 10, 20, 30, 40, 50, 60,70, 80, 90, 100
+    Print the contents of the text file
+    Add 101, 102, 103, 104, 105 to the list
+    Print contents of the file again
+"""
+
+
+def main():
+    def write_nums(thisFile, num1, num2, step):
+        for num in range(num1, num2+1, step):  # Use a loop to add numbers, in intervals of 'step'      #2,#4
+            thisFile.write(str(num) + "\n")
+
+    def read_file(thisFile):
+        file.seek(0)  # Move reading to line 0
+        print(file.readlines())  # Read all lines of the file                                #3
+
+    file = open("file.txt", 'w')        # Create File.txt                                           #1
+    file.close()                        # Close File.txt
+    file = open("file.txt","r+")        # Open File.txt for reading and writing
+    write_nums(file,10,100,10)
+    read_file(file)
+    write_nums(file,101,105,1)
+    read_file(file)
+    file.close()                        # Final file closing
+
+main()
