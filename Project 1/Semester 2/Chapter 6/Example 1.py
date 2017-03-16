e3fe594cd1f934b9d575f4a427aad46f132387df
Sean Curley <seancurley007@gmail.com>
2017-03-15 18:04:43 -0700

    Updated Functions, added ready_file()

--- Project 1/Semester 2/Chapter 6/Example 1.py
+++ Project 1/Semester 2/Chapter 6/Example 1.py
@@ -6,19 +6,26 @@
     Print contents of the file again
 """
 
+global file = ''
 
-def main():
-    def write_nums(thisFile, num1, num2, step):
-        for num in range(num1, num2+1, step):  # Use a loop to add numbers, in intervals of 'step'      #2,#4
-            thisFile.write(str(num) + "\n")
-
-    def read_file(thisFile):
-        file.seek(0)  # Move reading to line 0
-        print(file.readlines())  # Read all lines of the file                                #3
-
+def ready_file():
+    global file
     file = open("file.txt", 'w')        # Create File.txt                                           #1
     file.close()                        # Close File.txt
     file = open("file.txt","r+")        # Open File.txt for reading and writing
+    
+def read_file(thisFile):
+    thisFile.seek(0)  # Move reading to line 0
+    print(thisFile.readlines())  # Read all lines of the file                                #3
+
+def write_nums(thisFile, num1, num2, step):
+    for num in range(num1, num2+1, step):  # Use a loop to add numbers, in intervals of 'step'      #2,#4
+    thisFile.write(str(num) + "\n")
+            
+            
+def main():
+    global file
+    ready_file()
     write_nums(file,10,100,10)
     read_file(file)
     write_nums(file,101,105,1)
